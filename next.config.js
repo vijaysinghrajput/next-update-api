db080249ad3c8d964830096d5bf4de880ead9a7f
Vijay Singh <43938982+vijaysinghrajput@users.noreply.github.com>
2025-11-03 18:35:39 +0530

    Disable ESLint during builds for deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
   images: {
     remotePatterns: [
       // R2 Storage
